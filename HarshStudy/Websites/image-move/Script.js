b1eccf9a157e7b8a835dd1a2489eafd07bc486d0
Harsh1-shahu <shahuharsh1@gmail.com>
2025-01-29 13:50:15 +0530

    commit change

--- HarshStudy/Websites/image-move/Script.js
+++ HarshStudy/Websites/image-move/Script.js
@@ -1,4 +1,4 @@
-window.addEventListener("mousemove", (details) => {
+  window.addEventListener("mousemove", (details) => {
     let rect = document.querySelector(".rect");
     let xval = gsap.utils.mapRange(0, window.innerWidth, 200+rect.getBoundingClientRect().width/2,window.innerWidth - (200+rect.getBoundingClientRect().width/2), details.clientX)
     gsap.to(".rect", {
